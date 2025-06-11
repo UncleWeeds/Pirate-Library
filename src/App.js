fd42a49f6a0b71419bfb8d974550b013fad553f7
Your Name <you@example.com>
2025-06-11 06:33:49 +0530

    Restore routing: add <Routes> and <Route> in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import CategoryPage from './components/CategoryPage';
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      {/* Home at “/” */}
+      <Route path="/" element={<HomePage />} />
+      {/* Category pages at “/someCategoryId” */}
+      <Route path="/:categoryId" element={<CategoryPage />} />
+    </Routes>
+  );
 }
