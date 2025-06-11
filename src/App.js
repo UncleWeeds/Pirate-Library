6c29132080eb73c3425c888eaaea254df9920b9f
Your Name <you@example.com>
2025-06-11 06:27:37 +0530

    Temp: render HomePage only to verify setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import CategoryPage from './components/CategoryPage';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <div
-        className="min-h-screen text-white"
-        style={{
-          backgroundImage: "url('/background.jpg')",
-          backgroundSize:    'cover',
-          backgroundPosition:'center',
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/:categoryId" element={<CategoryPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  return <HomePage />;
 }
